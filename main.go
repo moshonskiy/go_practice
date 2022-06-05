e69ebe32072f3dc3cb8a59206d0535bc7dc9c7a7
Kirill Moshonskiy <furiousbenjamin@gmail.com>
2022-06-05 13:41:28 +0300

    first go commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var confernceName = "Go conference"
+
+	fmt.Println("Hello",  confernceName, "world")
+	fmt.Println("get your world")
+
+	fmt.Println(confernceName)
+
+}
